1848f1d8734419576ce8bc182faeed6d312d485a
alexandregama <alexandre.gama.lima@gmail.com>
2015-12-22 18:38:59 -0200

    Adds a new class to test Optional class

---
+++ src/test/java/br/com/moneyapi/java8/Pessoa.java
@@ -0,0 +1,15 @@
+package br.com.moneyapi.java8;
+
+public class Pessoa {
+
+	private String nome;
+
+	public Pessoa(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+}
